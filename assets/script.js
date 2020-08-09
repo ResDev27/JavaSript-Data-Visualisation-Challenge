fc6bf08677f404d89fff6b21d7323716a456fba1
Resa Quinta <resa.quinta@gmail.com>
2020-08-10 01:07:26 +0200

    chart 3

--- assets/script.js
+++ assets/script.js
@@ -26,3 +26,92 @@ document.getElementById(homicide).appendChild(newParagraph3);
 newParagraph3.innerHTML = chartValue3;
 
 
+
+
+
+
+
+
+
+
+
+/* 3rd Chart */
+let tableChart3 = document.getElementById("table2");
+// Testing purpose: console.log(tableChart3);
+
+const headersChart3 = []; // Will contain the header of chart (N°, Country, etc ..)
+const jsonChart3 = []; // Will contain all datas
+
+/*
+    Listing of all the headers, using a *for* loop
+*/
+for (let x = 0; x < tableChart3.rows[0].cells.length; x++)
+    {
+        headersChart3[x] = tableChart3.rows[0].cells[x].innerHTML; // Going through each cell of the first row
+    }
+// Testing purpose: console.log(headersChart3);
+
+/*
+    *for* loop getting datas based on row
+*/
+for ( let x = 1; x < tableChart3.rows.length; x++) // Starting @ 'x=1' because we already have the first (0) row from previous loop
+    {
+        let tableRow = tableChart3.rows[x];
+        let rowValues = {};
+        for (let y = 0; y < tableRow.cells.length; y++)
+            {
+                rowValues[headersChart3[y]] = tableRow.cells[y].innerHTML
+                .replace(/\n/g, '')
+                .replace(/<div>/g, '')
+                .replace(/<\/div>/g, '')
+                .replace(/<br>/g, ' ')
+                .replace(/\(.\)/g, '')
+                .replace(/\s/g, '');
+            }
+            /*
+                Use of .replace here to remove unwanted characters (based on regex rules, using: https://www.regextester.com/)
+            */
+        jsonChart3.push(rowValues); // Each row is pushed when last cell is reached (Y loop ending)
+}
+// Testing purpose: console.log(jsonChart3);
+
+const countryList = jsonChart3.map(function (f) // .map is used to have a new array listed by a function
+    {
+        return f.Country; // .map here is taking only Country into jsonChart3 array and return each Country occurence
+    }
+);
+// Testing purpose: console.log(countryList);
+
+const firstMillenial = jsonChart3.map(function (f) 
+    {
+        return parseInt(f["2007–09"]); // parseInt is here to convert "string" into "number"
+    }
+);
+// Testing purpose: console.log(firstMillenial);
+
+    const secondMillenial = jsonChart3.map(function (f) 
+    {
+        return parseInt(f["2010–12"]); // same as previous
+    }
+);
+// Testing purpose: console.log(secondMillenial);
+
+let thirdChart = document.getElementById('chart3');
+let myThirdChart = new Chart(thirdChart, {
+    type: 'bar',
+    data: {
+        labels: countryList,
+        datasets: [
+            {
+                label: "2007-09",
+                backgroundColor: "#2080FF",
+                data: firstMillenial,
+            },
+            {
+                label: "2010-12",
+                backgroundColor: "#FF2080",
+                data: secondMillenial,
+            },
+        ]
+    }
+});
